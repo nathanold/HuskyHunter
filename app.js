4e9964509b42d56d9a106edfd37caf1a58a18f1b
noldakowski <oldakowski.n@husky.neu.edu>
2017-10-22 16:07:22 -0400

    herokuu

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var connectionString = 'mongodb://localhost/huskyhunt';
 if (process.env.MLAB_USERNAME) { // check if running remotely
     connectionString += 'mongodb://' + process.env.MLAB_USERNAME + ':' + process.env.MLAB_PASSWORD + '@ds229835.mlab.com:29835/heroku_ncwj1xvn';
 }
-mongoose.connect(connectionString);
+//mongoose.connect(connectionString);
 require('./public/server/services/control.service.server.js');
