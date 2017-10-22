140c43867f6ddfb4f56427285faf1cd35a423073
noldakowski <oldakowski.n@husky.neu.edu>
2017-10-22 16:06:11 -0400

    connection to mlab

--- app.js
+++ app.js
@@ -2,20 +2,8 @@ console.log("SERVER SIDE!");
 var app = require('./express'); // creates an instance of the express lib
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://localhost/huskyhunt';
-if (process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
-}
-
-var connectionString = 'mongodb://localhost/huskyhunt'; // for local
 if (process.env.MLAB_USERNAME) { // check if running remotely
-    var username = process.env.MLAB_USERNAME; // get from environment
-    var password = process.env.MLAB_PASSWORD;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += 'ds139362.mlab.com:39362/heroku_nz263cn6'; // user yours
+    connectionString += 'mongodb://' + process.env.MLAB_USERNAME + ':' + process.env.MLAB_PASSWORD + '@ds229835.mlab.com:29835/heroku_ncwj1xvn';
 }
 mongoose.connect(connectionString);
 require('./public/server/services/control.service.server.js');
