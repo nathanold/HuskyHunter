ed05edd424154dfb8074a0e02f6e3fa13ec4aa0c
noldakowski <oldakowski.n@husky.neu.edu>
2017-10-28 14:20:22 -0400

    Dropbox

--- public/main/field/controllers/upload.controller.client.js
+++ public/main/field/controllers/upload.controller.client.js
@@ -11,5 +11,29 @@
         console.log("set number" + model.setNumber);
         console.log("clue number" + model.clueNumber);
         console.log("submit " + model.apiUrl);
+
+        model.uploadFile = function () {
+            console.log('uploading file');
+            var ACCESS_TOKEN = document.getElementById('access-token').value;
+            var dbx = new Dropbox({accessToken: ACCESS_TOKEN});
+            var fileInput = document.getElementById('file-upload');
+            var file = fileInput.files[0];
+            var originalExtension = file.name.split(".")[1];
+            console.log(originalExtension);
+            var fileName = model.setNumber + model.clueNumber + "." + originalExtension;
+            console.log(fileName);
+            file.name = fileName;
+            console.log(file);
+            dbx.filesUpload({path: '/' + fileName, contents: file})
+                .then(function (response) {
+                    var results = document.getElementById('results');
+                    results.appendChild(document.createTextNode('File uploaded!'));
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+            return false;
+        }
     }
 })();
--- server.js
+++ server.js
@@ -2,17 +2,17 @@ var app = require('./express'); // creates an instance of the express lib
 var express = app.express;
 
 var cookieParser = require('cookie-parser');
-var session      = require('express-session');
+var session = require('express-session');
 var passport = require('passport');
-
+var dbx = require('dropbox');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public/main'));
 app.use(cookieParser());
-app.use(session({ secret: "this is secret" }));
+app.use(session({secret: "this is secret"}));
 app.use(passport.initialize());
 app.use(passport.session());
 require('./public/server/services/control.service.server.js');
@@ -20,7 +20,7 @@ require('./public/server/services/upload.service.server.js');
 require('./public/server/services/download.service.server.js');
 require('./public/server/services/user.service.server.js');
 
-require ("./app.js");
+require("./app.js");
 
 var port = process.env.PORT || 3000;
 
